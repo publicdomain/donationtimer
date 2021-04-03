70825c3b8aa31f0cf025c545698779a217dbb0cc
Victor <publicdomainvic@gmail.com>
2021-04-03 00:38:47 -0400

    Add project files

---
+++ DonationTimer/DonationTimer/Program.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Program.cs" company="PUblicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace DonationTimer
+{
+    // Directives
+    using System;
+
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            // TODO Add code
+        }
+    }
+}
